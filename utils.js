d9542f7017a63b33b8c649dcf735fa6a31251c22
Brian Lin <me@brianl.in>
2017-01-21 19:38:10 -0800

    documentation

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
-//http://stackoverflow.com/questions/13771328/jquery-only-run-the-callback-function-once-after-all-the-event-changes
+// regex function to grab whatever we need from the page
+// Source: http://stackoverflow.com/questions/13771328/jquery-only-run-the-callback-function-once-after-all-the-event-changes
 jQuery.expr[':'].regex = function(elem, index, match) {
     var matchParams = match[3].split(','),
         validLabels = /^(data|css):/,
